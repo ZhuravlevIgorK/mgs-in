7009818d0d619870b166ec5c226d51931851446b
ZhuravlevIgorK <igor9851@rambler.ru>
2023-11-12 15:13:53 +0300

    start12-5

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -29,7 +29,7 @@ export default function contacts() {
             </div> 
 
             <footer style={{color:"white"}}>          
-                <TelegramBot/>
+                {/* <TelegramBot/> */}
             </footer>  
         </div>       
     )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
         <Slider/>
       </div>      
       
-      <footer>          
+      <footer>      fuu    
         {/* <TelegramBot/> */}
       </footer>   
 
